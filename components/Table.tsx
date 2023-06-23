02a5af3fbc718a12e27ca060cdd941300edf75af
Laura Davies <lauradaviesdev@gmail.com>
2023-06-23 16:20:44 +1000

    Make mobile responsive

--- components/Table.tsx
+++ components/Table.tsx
@@ -13,7 +13,7 @@ import {
 
 /* data needs to have any type, rather than ElectricityData to prevent Typescript throwing an error for react-table */
 const Table = ({ columns, data }: { columns: Columns[]; data: any }) => {
-  const PAGE_SIZE = 25;
+  const PAGE_SIZE = 5;
   const {
     getTableProps,
     getTableBodyProps,
@@ -58,56 +58,58 @@ const Table = ({ columns, data }: { columns: Columns[]; data: any }) => {
 
   return (
     <div className="flex items-center flex-col justify-center p-6">
-      <table
-        {...getTableProps()}
-        className="overflow-scroll w-3/4 h-3/4 table-auto text-left bg-[#FFFFFF] rounded-lg shadow-md"
-      >
-        <thead>
-          <tr>
-            {headerGroups.map((headerGroup) =>
-              headerGroup.headers.map((column, idx) => (
-                <th
-                  {...column.getHeaderProps()}
-                  key={idx}
-                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-                >
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-bold leading-none opacity-70"
-                  >
-                    {column.render("Header")}
-                  </Typography>
-                </th>
-              ))
-            )}
-          </tr>
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {page.map((row, idx) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()} key={idx}>
-                {row.cells.map((cell, idx) => (
-                  <td
-                    {...cell.getCellProps()}
+      <div className="overflow-x-auto w-full">
+        <table
+          {...getTableProps()}
+          className="w-full table-auto text-left bg-[#FFFFFF] rounded-lg shadow-md border-2"
+        >
+          <thead>
+            <tr>
+              {headerGroups.map((headerGroup) =>
+                headerGroup.headers.map((column, idx) => (
+                  <th
+                    {...column.getHeaderProps()}
                     key={idx}
-                    className="p-4 border-b border-blue-gray-50"
+                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4 bg-[#F5F8FB]"
                   >
                     <Typography
                       variant="small"
                       color="blue-gray"
-                      className="font-normal"
+                      className="font-bold leading-none opacity-70"
                     >
-                      {formatCellData(cell)}
+                      {column.render("Header")}
                     </Typography>
-                  </td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                  </th>
+                ))
+              )}
+            </tr>
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row, idx) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()} key={idx}>
+                  {row.cells.map((cell, idx) => (
+                    <td
+                      {...cell.getCellProps()}
+                      key={idx}
+                      className="p-4 border-b border-blue-gray-50"
+                    >
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {formatCellData(cell)}
+                      </Typography>
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
 
       <div className="flex justify-between p-5 m-5">
         <div>
@@ -115,11 +117,19 @@ const Table = ({ columns, data }: { columns: Columns[]; data: any }) => {
             {(pageIndex + 1) * PAGE_SIZE} of {data.length} results
           </p>
         </div>
-        <div className="ml-3 bg-[#FFFFFF] space-x-3 mx-2 px-1 text-[#5A6170] rounded-md border">
-          <button onClick={previousPage} disabled={!canPreviousPage}>
+        <div className="ml-3 space-x-3">
+          <button
+            onClick={previousPage}
+            disabled={!canPreviousPage}
+            className="btn-pagination"
+          >
             &lt;
           </button>
-          <button onClick={nextPage} disabled={!canNextPage}>
+          <button
+            onClick={nextPage}
+            disabled={!canNextPage}
+            className="btn-pagination"
+          >
             &gt;
           </button>
         </div>
